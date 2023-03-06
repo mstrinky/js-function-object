84bcdcd7e20bc2630f8e20c6fd4a6f026cb697e4
Mst Rinky Akter <mstrinkya@gmail.com>
2023-03-06 23:48:59 +0600

    return from a function

---
+++ return.js
@@ -0,0 +1,17 @@
+// function bringSingara (money) {
+//     var singaraPrice = 10;
+//     var quantity = money/singaraPrice;
+//     return quantity;
+// }
+
+//  var singaras =birngSingara(100);
+// console.log(singaras);
+
+
+function bringSingara(money){
+    var singaraPrice = 10;
+    var quantity = money/singaraPrice;
+    return quantity;
+}
+var singaras = bringSingara(100);
+console.log(singaras);
