450b0c3839922c9301c728a730a1526e75b3dc8c
Mst Rinky Akter <mstrinkya@gmail.com>
2023-03-06 22:48:56 +0600

    function-perametter

--- function.js
+++ function.js
@@ -1,1 +1,14 @@
-function
+// function declaration 
+function startFan(){
+    console.log('stand up');
+    console.log('walk towards the switch');
+    console.log('press the switch');
+}
+
+// call the function 
+startFan();
+
+function bringSingara(money){
+    console.log(money);
+}
+bringSingara(5);
