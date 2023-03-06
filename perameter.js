65b009e300c51f1bf60bbf2fd47872642bb7ae1e
Mst Rinky Akter <mstrinkya@gmail.com>
2023-03-06 23:16:41 +0600

    perameter and addition

---
+++ perameter.js
@@ -0,0 +1,13 @@
+function bringSingara(money){
+    console.log(money ,'tk disen')
+    console.log('ai nen singara');
+}
+bringSingara(5);
+
+
+function sum(a,b,c,d){
+    console.log(a,b,c,d);
+    var sum = a+b+c+d;
+    console.log(sum);
+}
+sum(1,2,3,4);
