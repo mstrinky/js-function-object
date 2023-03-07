8100fcdb7799f4d131100b042d394df724c9574f
Mst Rinky Akter <mstrinkya@gmail.com>
2023-03-07 21:49:52 +0600

    declare multiple objects with multiple properties

---
+++ object.js
@@ -0,0 +1,10 @@
+var myComputer = {
+    brand :'gigasonic',
+    color : 'black',
+    price : 32000,
+    processor : 'i3'
+}
+console.log(myComputer);
+console.log(myComputer.color);
+myComputer.brand = 'lenovo';
+console.log(myComputer);
