9cab5d93fc0a6d9c75647a0f7183246b655502b5
Mst Rinky Akter <mstrinkya@gmail.com>
2023-03-07 23:00:09 +0600

    else-if-switch

---
+++ else-if.js
@@ -0,0 +1,31 @@
+// else if 
+
+var color = 'orange';
+if (color=='red'){
+    console.log('you are my red friend');
+}
+else if (color=='white'){
+    console.log('you are my white friend');
+}
+else if (color=='orange'){
+    console.log('you are my orange friend');
+}
+else {
+    console.log('you are my black friend');
+}
+
+// switch 
+
+switch (color){
+    case 'green':
+        console.log('you are my green friend');
+        break;
+        case 'blue':
+            console.log('you are my blue friend');
+            break;
+            case 'orange':
+                console.log('you are my orange friend');
+                break;
+                default:
+                    console.log('you are my kala friend');
+}
