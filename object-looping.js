43879c524f2fe6ab614fc5229b96af90051ca475
Mst Rinky Akter <mstrinkya@gmail.com>
2023-03-07 22:37:14 +0600

    object keys and values

---
+++ object-looping.js
@@ -0,0 +1,18 @@
+
+
+// var friendAge = []
+
+var shopingCart = {
+    books:3,
+    sunglass:1,
+    keyboard:5,
+    mouse:1,
+    pen:25,
+    shoes:2
+}
+
+console.log(shopingCart);
+var keys = Object.keys(shopingCart);
+console.log(keys);
+var values = Object.values(shopingCart);
+console.log(values);
